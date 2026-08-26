ecc49fc17452787e2ec5fc1db690447fabf039ca
Sonakshi <hisonakshihere@gmail.com>
2026-08-26 16:35:26 +0530

    Modified App.js

--- Episode-1/App.js
+++ Episode-1/App.js
@@ -15,21 +15,20 @@ import ReactDOM from "react-dom/client";
 </div>
 */
 
+const heading = React.createElement("h1", { id: "parent" }, [
 
-const heading = React.createElement("h1",{id : "parent"},[
-//Child - 1
-    React.createElement("div",{id : "child"},[
-        React.createElement("h1",{},"I am h1 tag"),
-        React.createElement("h2",{},"I am h2 tag"),
-    ]),
-// Child - 2
-    React.createElement("div",{id : "child"},[
-        React.createElement("h1",{},"I am h1 tag"),
-        React.createElement("h2",{},"I am h2 tag"),
-    ]),
+  // Child 1
+  React.createElement("div", { id: "child1", key: "child1" }, [
+      React.createElement("h1", { id: "h1-1", key: "h1-1" }, "I am h1 tag"),
+      React.createElement("h2", { id: "h2-1", key: "h2-1" }, "I am h2 tag"),
+  ]),
 
-  ],
-);
+  // Child 2
+  React.createElement("div", { id: "child2", key: "child2" }, [
+      React.createElement("h1", { id: "h1-2", key: "h1-2" }, "I am h1 tag"),
+      React.createElement("h2", { id: "h2-2", key: "h2-2" }, "I am h2 tag"),
+  ]),
+]);
 
     const root = ReactDOM.createRoot(document.getElementById("container"));
     root.render(heading);
