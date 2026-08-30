0376ff72864f5dc5693d136882e8f5262ec03e33
Sonakshi <hisonakshihere@gmail.com>
2026-08-31 00:50:24 +0530

    S-P restaurant.js

---
+++ SwiggyProject/components/Restaurant.js
@@ -0,0 +1,13 @@
+export const RestauntCard = ({resData}) => {
+    const { name,cuisines, locality, avgRating }=resData?.info;
+
+    return(
+        <div className="res-card">
+           <img src={`https://media-assets.swiggy.com/swiggy/image/upload/${resData.info.cloudinaryImageId}`} alt={resData.info.name} />
+            <h3>{name}</h3> 
+            <h4>{cuisines.join(",")}</h4>
+            <h5>{avgRating}</h5> <br/>
+            <h5>{locality}</h5>
+        </div>
+    )
+}
