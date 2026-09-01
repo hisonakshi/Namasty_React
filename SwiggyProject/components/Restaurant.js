3b71847354d36d0e79207e7d800ea101602ef8d3
Sonakshi <hisonakshihere@gmail.com>
2026-09-01 11:15:58 +0530

    updated Restaurant.js

--- SwiggyProject/components/Restaurant.js
+++ SwiggyProject/components/Restaurant.js
@@ -1,9 +1,11 @@
+import {CDN_URL} from "../utils/constants.js";
+
 export const RestauntCard = ({resData}) => {
     const { name,cuisines, locality, avgRating }=resData?.info;
 
     return(
         <div className="res-card">
-           <img src={`https://media-assets.swiggy.com/swiggy/image/upload/${resData.info.cloudinaryImageId}`} alt={resData.info.name} />
+           <img src={CDN_URL+ `${resData.info.cloudinaryImageId}`} alt={resData.info.name} />
             <h3>{name}</h3> 
             <h4>{cuisines.join(",")}</h4>
             <h5>{avgRating}</h5> <br/>
