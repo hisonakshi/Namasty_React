642fe70f6eaa734bc92babf628b4769f9a0eed06
Sonakshi <hisonakshihere@gmail.com>
2026-09-08 20:30:33 +0530

    update shimmer

--- SwiggyProject/components/Shimmer.js
+++ SwiggyProject/components/Shimmer.js
@@ -9,6 +9,11 @@ export const Shimmer = () => {
             <div className="shimmer-card"></div>
             <div className="shimmer-card"></div>
             <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"> </div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
         </div>
     );
 };
+
