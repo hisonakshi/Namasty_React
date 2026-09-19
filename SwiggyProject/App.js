e6a0bd2baf8a6fb320d7def42de73929d77eafb2
Sonakshi <hisonakshihere@gmail.com>
2026-09-19 21:52:54 +0530

    Parent Class based component

--- SwiggyProject/App.js
+++ SwiggyProject/App.js
@@ -6,9 +6,9 @@ import {About} from "./components/About";
 import {Contact} from "./components/Contact";
 import {Error} from "./components/Error";
 import {RestaurantMenu} from "./components/RestaurantMenu";
-
-
 import {createBrowserRouter , RouterProvider , Outlet} from "react-router-dom";
+
+
 
 const AppLayout = () => {
     return(
