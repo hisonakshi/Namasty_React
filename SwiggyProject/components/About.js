d65d45dd8f68914a464d5ab133e52595c12b7f79
Sonakshi <hisonakshihere@gmail.com>
2026-09-19 21:55:59 +0530

    Parent class component

--- SwiggyProject/components/About.js
+++ SwiggyProject/components/About.js
@@ -1,8 +1,54 @@
-export const About = () => {
-    return(
-      <div>
-        <h1>About page</h1>
-        <h3>This is Namasty React series</h3>
-      </div>
-    )
+import { User } from "./User";
+import { UserClass } from "./UserClass";
+import {Component} from "react";
+
+// Parent component 
+
+export class About extends Component{
+
+  constructor(props){
+    super(props);
+
+    console.log("Parent Constructor");
+  }
+
+  componentDidMount(){
+    console.log("parent component did mount");
+  }
+  
+  render(){
+    console.log("Render");
+
+    return (
+              <div>
+                  <h1>About Class Components</h1>
+                  <h3>This is Namasty React series</h3>
+                  <User name="Sonakshi" location="Delhi (Function)"  />
+                  <UserClass name="Sonakshi" location="Delhi (Class Based)"  />
+              </div>
+          );
+  }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// export const About = () => {
+//     return (
+//         <div>
+//             <h1>About page</h1>
+//             <h3>This is Namasty React series</h3>
+//             <User name="Sonakshi" location="Delhi (Function)"  />
+//             <UserClass name="Sonakshi" location="Delhi (Class Based)"  />
+//         </div>
+//     );
+// };
